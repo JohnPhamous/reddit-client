61851ed756b2e5d38ed78eff00991f142822681c
John Pham <johnphammail@gmail.com>
2020-09-08 22:09:43 -0700

    Add voting styles

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Post.css';
 import {
   TiArrowUpOutline,
@@ -9,15 +9,56 @@ import {
 import timeAgo from '../../utils/timeAgo';
 
 const Post = (props) => {
+  const [voteValue, setVoteValue] = useState(0);
+
   const { post } = props;
   const { data } = post;
+
+  /**
+   * @param {number} newValue The new vote value
+   */
+  const onHandleVote = (newValue) => {
+    if (newValue === voteValue) {
+      setVoteValue(0);
+    } else if (newValue === 1) {
+      setVoteValue(1);
+    } else {
+      setVoteValue(-1);
+    }
+  };
+
+  const renderUpVote = () => {
+    if (voteValue === 1) {
+      return <TiArrowUpThick className="post-votes-action up-vote" />;
+    }
+    return <TiArrowUpOutline className="post-votes-action" />;
+  };
+
+  const renderDownVote = () => {
+    if (voteValue === -1) {
+      return <TiArrowDownThick className="post-votes-action down-vote" />;
+    }
+    return <TiArrowDownOutline className="post-votes-action" />;
+  };
 
   return (
     <article className="post" key={data.id}>
       <div className="post-votes-container">
-        <TiArrowUpOutline className="post-votes-action" />
+        <button
+          type="button"
+          className="post-votes-button"
+          onClick={() => onHandleVote(1)}
+        >
+          {renderUpVote()}
+        </button>
         <p className="post-votes-value">{data.ups}</p>
-        <TiArrowDownOutline className="post-votes-action" />
+        <button
+          type="button"
+          className="post-votes-button"
+          onClick={() => onHandleVote(-1)}
+        >
+          {renderDownVote()}
+        </button>
       </div>
       <div className="post-container">
         <h3 className="post-title">{data.title}</h3>
