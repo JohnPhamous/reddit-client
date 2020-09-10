12333faeed3168c2c381d065063518c6fd5680d4
John Pham <johnphammail@gmail.com>
2020-09-09 19:05:54 -0700

    Add loading screen

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Post from '../Post/Post';
+import PostLoading from '../Post/PostLoading';
+import getRandomNumber from '../../utils/getRandomNumber';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
+      setIsLoading(true);
       const response = await fetch('https://www.reddit.com/r/pics.json');
       const json = await response.json();
       setPosts(json.data.children);
+      setIsLoading(false);
     };
 
     fetchPosts();
   }, []);
+
+  if (isLoading) {
+    return Array(getRandomNumber(3, 10)).fill(<PostLoading />);
+  }
 
   return (
     <>
