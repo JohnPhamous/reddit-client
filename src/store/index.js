7fdbbd66c5fc2cfad75960d19fe37221f0351447
John Pham <johnphammail@gmail.com>
2020-09-12 13:38:45 -0700

    Remove counter slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import redditReducer from './redditSlice';
 
 export default configureStore({
   reducer: combineReducers({
-    counter: counterReducer,
     reddit: redditReducer,
   }),
 });
